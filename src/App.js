9d58870a2a4c5baaa4a426efa255978d25a19f5d
Lilian Giselly <lgps@ic.ufal.br>
2020-08-30 01:42:25 -0300

    2h36min05seg

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -26,8 +28,26 @@ export default () => {
 
     loadAll();
   }, []);
+
+  useEffect(()=>{
+      const scrollListener = () => {
+        if(window.scrollY > 10){
+          setBlackHeader(true)
+        }
+        else
+          setBlackHeader(false);
+      }
+
+      window.addEventListener('scroll', scrollListener);
+
+      return () => {
+        window.removeEventListener('scroll', scrollListener);
+      }
+  }, [])
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
 
       {featuredData &&
         <FeaturedMovie item={featuredData} />  
@@ -38,6 +58,11 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items}/>
         ))}
       </section>
+      
+      <footer>
+          Direitos de imagem para Netflix<br/>
+          Dados pegos do site TMDB.
+      </footer>
     </div>
   );
 }
